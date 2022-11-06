dc5e270e6fced2317d89a4d66f16967745735759
Bartosz Janusz <bartosz.yan@gmail.com>
2022-11-06 09:40:52 +0100

    eslint ts test

--- src/components/editable/Editable.tsx
+++ src/components/editable/Editable.tsx
@@ -8,12 +8,13 @@ interface Props {
 
 const Editable: React.FC<Props> = ({ handleChange, children }): React.ReactElement => {
   const element = useRef<any>();
-  let elements = React.Children.toArray(children);
+  let elements: (string | number | React.ReactElement<any, string | React.JSXElementConstructor<any>> | React.ReactFragment | React.ReactPortal)[] = React.Children.toArray(children);
   if (elements.length > 1) {
     throw Error("Can't have more than one child");
   }
   const onBlur = () => {
     const value = element.current?.value || element.current?.innerText;
+    // eslint-disable-next-line @typescript-eslint/strict-boolean-expressions
     if (handleChange) {
       handleChange(element, value);
     }
